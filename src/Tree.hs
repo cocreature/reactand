fbbb2c77e1888a71986407a9fdaeb2e8adea7841
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-05-31 13:22:26 +0200

    Document Tree modulec

--- src/Tree.hs
+++ src/Tree.hs
@@ -8,7 +8,6 @@ module Tree
   ,TreeZipper(..)
   ,integrate
   ,integrate'
-  ,createZipper
   ,insertUp
   ,focusL
   ,leftL
@@ -27,13 +26,14 @@ module Tree
 import Text.PrettyPrint.HughesPJClass
 import Control.Lens
 
--- | l is an annotation at a branch
--- | a is the type of a leaf
+-- | l is an annotation at a branch (usually the layout)
+-- | a is the type of a leaf (usually a window)
 data Tree l a =
   Tree {_layout :: !l
        ,_treeElements :: !(Maybe (ListZipper (Either a (Tree l a))))}
   deriving (Show,Read,Eq,Ord)
 
+-- | one focused element and some elements on both sides
 data ListZipper a =
   ListZipper {_focusL :: !a
              ,_leftL :: ![a]
@@ -54,6 +54,7 @@ instance Pretty a => Pretty (ListZipper a) where
 makeLenses ''Tree
 makeLenses ''ListZipper
 
+-- | one focused tree and the parent elements of that tree
 data TreeZipper l a =
   TreeZipper {_focusT :: !(Tree l a)
              ,_parentsT :: ![([Either a (Tree l a)],l,[Either a (Tree l a)])]}
@@ -73,14 +74,12 @@ instance (Pretty l,Pretty (ListZipper (Either a (Tree l a)))) => Pretty (Tree l 
 
 makeLenses ''TreeZipper
 
-createZipper :: Tree a l -> TreeZipper a l
-createZipper t = TreeZipper t []
-
+-- | insert an element in a list zipper
 insertUpList :: a -> Maybe (ListZipper a) -> (ListZipper a)
 insertUpList a Nothing = ListZipper a [] []
 insertUpList a (Just (ListZipper f l r)) = ListZipper a l (f:r)
 
--- if a leaf is focused split it up and use the supplied annotation
+-- | if a leaf is focused split it up and use the supplied annotation
 -- otherwise just move the current focus to the right and insert
 insertUp :: l -> a -> TreeZipper l a -> TreeZipper l a
 insertUp _ el z =
@@ -94,6 +93,7 @@ integrate (ListZipper x l r) = reverse l ++ x : r
 integrate' :: Maybe (ListZipper a) -> [a]
 integrate' = maybe [] integrate
 
+-- | reconstruct the complete tree from a tree zipper
 integrateTree :: TreeZipper l a -> Tree l a
 integrateTree (TreeZipper f []) = f
 integrateTree (TreeZipper f ((ls,l,rs):xs)) =
@@ -104,6 +104,7 @@ integrateTree (TreeZipper f ((ls,l,rs):xs)) =
                          ls
                          rs))
 
+-- | create a new tree containing only the focused element
 split :: TreeZipper l a -> TreeZipper l a
 split (TreeZipper (Tree l Nothing) p) = TreeZipper (Tree l Nothing) p
 split (TreeZipper (Tree l (Just (ListZipper f ls rs))) ps) =
@@ -113,11 +114,13 @@ split (TreeZipper (Tree l (Just (ListZipper f ls rs))) ps) =
                             ls
                             rs))) ps
 
+-- | move down in the tree
 moveDown :: TreeZipper l a -> TreeZipper l a
 moveDown (TreeZipper (Tree l Nothing) ps) = TreeZipper (Tree l Nothing) ps
 moveDown (TreeZipper (Tree l (Just (ListZipper (Left f) ls rs))) ps) = TreeZipper (Tree l (Just (ListZipper (Left f) ls rs))) ps
 moveDown (TreeZipper (Tree l (Just (ListZipper (Right t) ls rs))) ps) = TreeZipper t ((ls,l,rs):ps)
 
+-- | move up in the tree
 moveUp :: TreeZipper l a -> TreeZipper l a
 moveUp (TreeZipper t []) = TreeZipper t []
 moveUp (TreeZipper t ((ls,l,rs):ps)) =
@@ -128,6 +131,7 @@ moveUp (TreeZipper t ((ls,l,rs):ps)) =
                             rs)))
     ps
 
+-- | move the focused view one level up and remove possibly empty trees
 moveViewUp :: TreeZipper l a -> TreeZipper l a
 moveViewUp tz@(TreeZipper _ []) = tz
 moveViewUp tz@(TreeZipper (Tree _ Nothing) _) = tz
@@ -146,6 +150,7 @@ moveViewUp (TreeZipper (Tree l (Just z)) ((ls,lp,rs):p)) =
                                 rs)))
         p
 
+-- | remove the focused element
 removeFocused :: ListZipper a -> Maybe (ListZipper a)
 removeFocused (ListZipper _ [] []) = Nothing
 removeFocused (ListZipper _ (x:xs) rs) = Just $ ListZipper x xs rs
